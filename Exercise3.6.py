fb9808dd475699547988fd5b7a98989c0695ee31
togarobaja <80144717+togarobaja@users.noreply.github.com>
2021-03-19 23:03:09 +0700

    Add files via upload

---
+++ Exercise3.6.py
@@ -0,0 +1,11 @@
+#TOGAR OBAJA NAINGGOLAN
+#NIM I0320103
+#KELAS C
+#Exercise 3.6
+
+##Update dictionary pyhton
+dict = {'NAme':'Zara','Age':7,'Class':'First'}
+dict['Age'] = 8;    # Mengubah entri yang sudah ada
+dict['School'] = "DPS School"   # Menambah entri baru
+print("dict['Age']:",dict['Age'])
+print("dict['School]:",dict['School'])
